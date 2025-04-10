4d24d85fc8af73dcb725e18d75a83df422b6c9d8
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-04-10 18:36:05 +0530

    feat: initialize React + TypeScript + Vite project with essential configurations

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom/client";
+import { SessionProvider } from "./providers/sessionProvider";
+import { App } from "./app";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider.Provider>
+        <App />
+      </SessionProvider.Provider>
+    </QueryClientProvider>
+  );
+}
---
+++ apps/web/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+      port: 3000,
+      host: '0.0.0.0',
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+  },
+});
