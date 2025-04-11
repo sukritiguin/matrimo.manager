80d886cb1c2a43e32910f6aa2f3f14a3d77ed86c
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-04-11 18:40:16 +0530

    refactor(auth, users): enhance error handling and logging in authentication and user retrieval processes

--- apps/api/src/controllers/users.controllers.ts
+++ apps/api/src/controllers/users.controllers.ts
@@ -1,30 +1,37 @@
 import { client } from "@matrimo/db";
 import { ApiError, apiHandler, ApiResponse } from "@matrimo/lib";
 import { Request, Response } from "express";
+import { logger } from "../utils/logger.js";
 
 const getMe = apiHandler(async (req: Request, res: Response) => {
-  const user = await client.user.findUnique({
-    where: {
-      id: req.currentUser?.id,
-    },
-    select: {
-      id: true,
-      name: true,
-      email: true,
-      username: true,
-      bio: true,
-      emailVerified: true,
-      onBoarding: true,
-      createdAt: true,
-      verified: true,
-    },
-  });
+  try {
+    const user = await client.user.findUnique({
+      where: {
+        id: req.currentUser?.id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        username: true,
+        bio: true,
+        emailVerified: true,
+        onBoarding: true,
+        createdAt: true,
+        verified: true,
+      },
+    });
 
-  if (!user) {
-    throw new ApiError(404, "User not found");
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
+
+    return res.status(200).json(new ApiResponse(true, "Ok", { user }));
+  } catch (error) {
+    if (error instanceof ApiError) throw error;
+    logger.debug("ERROR IN GET ME", error);
+    throw new ApiError(500, "Something went wrong");
   }
-
-  return res.status(200).json(new ApiResponse(true, "Ok", { user }));
 });
 
 export { getMe };
