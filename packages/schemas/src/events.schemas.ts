229f7041b44d92fa535e8e438142af789828cf3a
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-04-15 17:12:25 +0530

    feat(schemas): add canvas and event schemas with validation

---
+++ packages/schemas/src/events.schemas.ts
@@ -0,0 +1,43 @@
+import z from "zod";
+import { requiredString } from "./common.js";
+import { createCanvasSchema } from "./canvas.schemas.js";
+
+const CategorySchema = z
+  .object({
+    type: z.string(),
+    other: z.string().optional(),
+  })
+  .transform((data) => ({
+    ...data,
+    type: data.type.toUpperCase(),
+  }))
+  .refine((data) => (data.type === "OTHER" ? !!data.other : true), {
+    message: "Other category must have a description",
+  });
+
+const createEventSchema = z.object({
+  title: requiredString("Title")
+    .min(3, "Title is too short")
+    .max(100, "Title is too long")
+    .default("Untitled Event"),
+  description: z.string().optional(),
+  tags: z
+    .array(
+      requiredString("Tag").min(3, "Tag must be at least 3 characters long")
+    )
+    .optional(),
+  category: CategorySchema,
+  canvas: createCanvasSchema.optional(),
+});
+
+const updateEventSchema = createEventSchema.partial();
+
+const eventParamsSchema = z.object({
+  eventId: requiredString("Event id").uuid("Event id must be a valid uuid"),
+});
+
+export { createEventSchema, updateEventSchema, eventParamsSchema };
+
+export type CreateEventSchema = z.infer<typeof createEventSchema>;
+export type UpdateEventSchema = z.infer<typeof updateEventSchema>;
+export type EventParamsSchema = z.infer<typeof eventParamsSchema>;
--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -1,2 +1,4 @@
 export * from "./auth.schemas";
 export * from "./auth.schemas.types";
+export * from "./events.schemas";
+export * from "./canvas.schemas";
